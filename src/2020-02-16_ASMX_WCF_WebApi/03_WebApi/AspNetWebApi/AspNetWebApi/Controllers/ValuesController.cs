c1c541390d9d68aac953178a1100fe4ca384bb32
hellomrsun <soleilcharles@gmail.com>
2020-02-16 18:05:54 +0100

    update web api

--- src/2020-02-16_ASMX_WCF_WebApi/03_WebApi/AspNetWebApi/AspNetWebApi/Controllers/ValuesController.cs
+++ src/2020-02-16_ASMX_WCF_WebApi/03_WebApi/AspNetWebApi/AspNetWebApi/Controllers/ValuesController.cs
@@ -1,5 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Web.Http;
+using System.Web.Http.Results;
 
 namespace AspNetWebApi.Controllers
 {
@@ -14,9 +18,12 @@ namespace AspNetWebApi.Controllers
         // GET api/values/5
         [HttpGet]
         [Route("api/v1/Values/{id}")]
-        public string Get(int id)
+        public HttpResponseMessage Get(int id)
         {
-            return $"input is {id}";
+            var content = "{ 'id';" + id + "}";
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, "value");
+            response.Content = new StringContent(content, Encoding.UTF8);
+            return response;
         }
 
         // POST api/values
