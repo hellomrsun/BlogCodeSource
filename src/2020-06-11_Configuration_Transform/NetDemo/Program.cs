218112a0c57c7533a47ece094c7677f19efc8ead
hellomrsun <soleilcharles@gmail.com>
2020-06-12 19:37:25 +0200

    Visual studio ConfigurationTransform .net framework demo project

---
+++ src/2020-06-11_Configuration_Transform/NetDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var appName = ConfigurationManager.AppSettings["ApplicationName"];
+            Console.WriteLine($"Application name is: {appName}.");
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
